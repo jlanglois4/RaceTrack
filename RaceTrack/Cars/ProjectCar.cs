6786d1de8fad84a44bd4ee503ce0803a43e06067
jlanglois4 <jlanglois4@my.wctc.edu>
2022-03-07 19:10:54 -0600

    Added new RacerCar ProjectCar Added new Driver JimmyJohn Added new RaceCar abstract method, StopEngine Implemented all abstract methods to ProjectCar Changed position of adding drivers in RaceTrack (Tractor behind Ford GT)

---
+++ RaceTrack/Cars/ProjectCar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class ProjectCar : RaceCar
+    {
+        public ProjectCar()
+        {
+            Name = "Crap Box";
+            TopSpeed = 500;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name}'s engine is crank-started and zooms off.");
+            Position = 100;
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} spins out of control upon crossing the finish line!");
+            base.Brake();
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"A hand is seen from the {Name}'s battered window, giving a thumbs up.");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/JimmyJohn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class JimmyJohn : Driver
+    {
+        public JimmyJohn(RaceCar car) : base(car)
+        {
+            Name = "Jimmy John";
+            SkillLevel = 2;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+    }
+}
